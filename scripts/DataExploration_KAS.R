bd3e277150943f21d1f7d250dd0d9f559d271ada
1010sheehy <kasheehy@ucdavis.edu>
2021-05-14 16:47:26 -0700

    Added data and started data exploration script
    Added data (Summer's csv in Drive) and started personal data exploration script

---
+++ scripts/DataExploration_KAS.R
@@ -0,0 +1,13 @@
+###
+### Infant - Aggression Data Exploration
+###
+
+#   Goals:
+#   - Propose a few general questions
+#   - Decide on response variable
+#   - Identify and describe potential pitfalls
+
+'Set filepath'
+
+monkey_agg <- read.csv("")
+
