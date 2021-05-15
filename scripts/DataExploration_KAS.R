82d921d7a7233e7475fa0d6015394aaa31754241
1010sheehy <kasheehy@ucdavis.edu>
2021-05-14 17:26:56 -0700

    Began exploring mom's agg rates
    Interested what the distribution of agg per day is. Seemed low (most less than one agg per day).
    
    Next steps: ID whether there are a few outliers (near zero).

--- scripts/DataExploration_KAS.R
+++ scripts/DataExploration_KAS.R
@@ -7,7 +7,24 @@
 #   - Decide on response variable
 #   - Identify and describe potential pitfalls
 
-'Set filepath'
+'Install Packages'
+library(tidyverse)
+library(ggplot2)
 
-monkey_agg <- read.csv("")
+'Import Data using GitHub permalink to "master" branch'
+monkey_agg <- read.csv("https://raw.githubusercontent.com/1010sheehy/EVE298_LMM_GroupProj/master/data/Infant_Conflict_Data_W_O_NAs.csv", header = TRUE)
+str(monkey_agg)
+summary(monkey_agg)
 
+### Exploratory Figures ----
+# > Mom's aggressive interactions ---- 
+# Looking at distribution of mom's involvement with aggression (w/ baby)
+#  - Noticed that the mean ratio was below 1 for both instigating and receiving
+#  - This means less than one (or two?) aggressive interactions per day
+
+ggplot(data = monkey_agg,
+       aes(y = Exp.1to4.i)) +
+  geom_boxplot()
+
+'Notes from looking at data:
+Exp.1to4.i'
